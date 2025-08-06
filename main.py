4372bb8eef4e2c5e80da41960d4eb604736dbe5e
chanders0 <christina.stiff@gmail.com>
2025-08-06 18:36:08 -0500

    Configure and move player

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,13 +13,27 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+
         screen.fill("black")
+        for shape in drawable:
+            shape.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
